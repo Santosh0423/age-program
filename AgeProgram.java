488af8d8fee3683029cd958ca59bf43631fc1185
Santosh Sigdel <santosh.sigdel900@gmail.com>
2025-10-22 11:42:16 +0300

    Initial commit: Add AgeProgram with age check

---
+++ AgeProgram.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AgeProgram {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // Ask user for their age
+        System.out.print("Enter your age: ");
+        int age = in.nextInt();
+
+        // Check age category
+        if (age < 13) {
+            System.out.println("You are a child.");
+        } else if (age < 20) {
+            System.out.println("You are a teenager.");
+        } else if (age < 65) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a senior.");
+        }
+
+        in.close();
+    }
+}
